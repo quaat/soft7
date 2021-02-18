ad5ebb06a3af1606b608649a383e534b807a163b
Treesa <treesarjc@gmail.com>
2021-02-18 21:34:04 +0100

    version file added and setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 import setuptools
+import subprocess
+import os
+
+soft7_version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+assert "." in soft7_version
+
+assert os.path.isfile("soft7/version.py")
+with open("soft7/VERSION", "w", encoding="utf-8") as fh:
+    fh.write(f"{soft7_version}\n")
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -18,5 +27,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
+    package_data={'soft7': ['VERSION']},
+    include_package_data=True,
     python_requires='>=3.7',
 )
